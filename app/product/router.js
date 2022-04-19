64f26d24166bba37274931e6440d74485155d050
Baimempatlima <97828149+baimempatlima@users.noreply.github.com>
2022-04-19 16:15:35 +0700

    Update router.js

--- app/product/router.js
+++ app/product/router.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const multer = require("multer");
 const os = require("os");
-const { police_check } = require("../../middleware");
+const { police_check } = require("../../middlewares");
 const productController = require("./controller");
 
 router.get("/products", productController.index);
