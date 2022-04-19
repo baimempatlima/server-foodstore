9a453b2bd6314b0f3be4bdc9f3cc91ad70311622
Baimempatlima <97828149+baimempatlima@users.noreply.github.com>
2022-04-19 14:13:34 +0700

    Update router.js

--- app/order/router.js
+++ app/order/router.js
@@ -6,6 +6,6 @@ router.post("/orders", police_check("create", "Order"), orderController.store);
 
 router.get("/orders", police_check("view", "Order"), orderController.index);
 
-router.get("/orders/:id", police_check("view", "Order"), orderController.show);
+// router.get("/orders/:id", police_check("view", "Order"), orderController.show);
 
 module.exports = router;
