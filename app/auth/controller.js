5bb029feb58d68faf2f5aefb6336a3ab22b47590
Baimempatlima <97828149+baimempatlima@users.noreply.github.com>
2022-04-30 03:45:25 +0700

    Update controller.js

--- app/auth/controller.js
+++ app/auth/controller.js
@@ -10,45 +10,47 @@ const register = async (req, res, next) => {
     const payload = req.body;
     let user = new User(payload);
     await user.save();
-    return res.status(200).json(user);
+    return res.json(user);
   } catch (err) {
     if (err && err.name === "ValidationError") {
-      return res.status(204).json({
+      return res.json({
         error: 1,
         message: err.message,
         fields: err.errors,
       });
     }
+
     next(err);
   }
 };
 
 const localStrategy = async (email, password, done) => {
   try {
-    let user = await User.findOne({ email }).select("-__v -createdAt -updatedAt -cart_items -token");
+    let user = await User.findOne({ email }).select("-__v -createdAt -updateAt -cart_items -token");
+
     if (!user) return done();
     if (bcrypt.compareSync(password, user.password)) {
-      ({ user, ...userWithoutPassword } = user.toJSON());
+      ({ password, ...userWithoutPassword } = user.toJSON());
       return done(null, userWithoutPassword);
     }
   } catch (err) {
     done(err, null);
   }
+
   done();
 };
 
-const login = async (req, res, next) => {
+const login = (req, res, next) => {
   passport.authenticate("local", async function (err, user) {
     if (err) return next(err);
-    if (!user) return res.status(200).json({ error: 1, message: "email or password incorect" });
+    if (!user) return res.json({ error: 1, message: "Email or password incorrect" });
 
     let signed = jwt.sign(user, config.secretKey);
-
     await User.findByIdAndUpdate(user._id, { $push: { token: signed } });
 
-    res.status(200).json({
+    res.json({
       message: "Login successfully",
-      user: { name: user.full_name, email: user.email, role: user.role },
+      user,
       token: signed,
     });
   })(req, res, next);
@@ -60,25 +62,33 @@ const logout = async (req, res, next) => {
   let user = await User.findOneAndUpdate({ token: { $in: [token] } }, { $pull: { token: token } }, { useFindAndModify: false });
 
   if (!token || !user) {
-    res.status(200).json({
+    res.json({
       error: 1,
-      message: "No User Found",
+      message: "No User Found!!",
     });
   }
-  return res.status(200).json({
+
+  return res.json({
     error: 0,
-    message: "logout berhasil",
+    message: "Logout berhasil",
   });
 };
 
 const me = (req, res, next) => {
   if (!req.user) {
-    res.status(200).json({
-      error: 1,
-      message: "you are not login or token expired",
+    res.json({
+      err: 1,
+      message: "You are not login or token expired",
     });
   }
-  res.status(200).json(req.user);
+  res.json(req.user);
 };
 
-module.exports = { register, login, localStrategy, logout, me };
+module.exports = {
+  register,
+  localStrategy,
+  login,
+  logout,
+  me,
+};
+
