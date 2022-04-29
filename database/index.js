1bfaad4b794c9a53fb54509961900d4a896451fa
Baimempatlima <97828149+baimempatlima@users.noreply.github.com>
2022-04-30 04:26:30 +0700

    Update index.js

--- database/index.js
+++ database/index.js
@@ -1,16 +1,8 @@
 const mongoose = require("mongoose");
 const { dbHost, dbPass, dbName, dbPort, dbUser } = require("../app/config");
 
-mongoose.connect(`mongodb+srv://${dbUser}:${dbPass}@${dbHost}/${dbName}?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true });
-// mongoose.connect(`mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?authSource=admin`);
+// mongoose.connect(`mongodb+srv://${dbUser}:${dbPass}@${dbHost}/${dbName}?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(`mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?authSource=admin`);
 const db = mongoose.connection;
-
-db.on('open', () => {
-    if(connectionDatabase !== urlLocal) {
-        console.log('connected to the mongodb atlas');
-    } else {
-        console.log('failed');
-    }
-})
 
 module.exports = db;
