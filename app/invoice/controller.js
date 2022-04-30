6f04e4c26a4cd4bcba441a06df6e41f71733624c
Baimempatlima <97828149+baimempatlima@users.noreply.github.com>
2022-04-30 17:29:07 +0700

    Update controller.js

--- app/invoice/controller.js
+++ app/invoice/controller.js
@@ -1,19 +1,16 @@
 const { subject } = require("@casl/ability");
 const Invoice = require("./model");
-const { policyfor } = require("../../utils");
+const { policyFor } = require("../../utils");
 
 const show = async (req, res, next) => {
   try {
     let { order_id } = req.params;
     // let invoice = await Invoice.findOne({ order: order_id });
-    let invoice = await Invoice
-        .findOne({order: order_id})
-        .populate('order')
-        .populate('user');
-    let policy = policyfor(req.user);
+    let invoice = await Invoice.findOne({ order: order_id }).populate("order").populate("user");
+    let policy = policyFor(req.user);
     let subjectInvoice = subject("Invoice", {
       ...invoice,
-      user_id: invoice.user,
+      user_id: invoice.user._id,
     });
     if (!policy.can("read", subjectInvoice)) {
       return res.json({
@@ -21,8 +18,8 @@ const show = async (req, res, next) => {
         message: "anda tidak memiliki akses untuk melihat invoice ini",
       });
     }
-    invoice = await Invoice.find({ order: order_id }).populate("order").populate("user");
-    return res.status(200).json(invoice);
+    // invoice = await Invoice.find({ order: order_id }).populate("order").populate("user");
+    return res.json(invoice);
   } catch (err) {
     return res.json({
       error: 1,
@@ -30,6 +27,27 @@ const show = async (req, res, next) => {
     });
   }
 };
+
+// const show = async (req, res, next) => {
+//   try {
+//     let policy = policyFor(req.user);
+//     let subjectInvoice = subject("Invoice", { ...invoice, user_id: invoice.user._id });
+//     if (!policy.can("read", subjectInvoice)) {
+//       return res.json({
+//         error: 1,
+//         message: `Anda tidak memiliki akses untuk melihat invoice ini`,
+//       });
+//     }
+//     let { order_id } = req.params;
+//     let invoice = await Invoice.findOne({ order: order_id }).populate("order").populate("user");
+//     return res.json(invoice);
+//   } catch (error) {
+//     return res.json({
+//       error: 1,
+//       message: `Error when getting invoice`,
+//     });
+//   }
+// };
 
 // const index = async (req, res, next) => {
 //   try {
@@ -44,9 +62,10 @@ const show = async (req, res, next) => {
 //       error: 1,
 //       message: "Error when getting invoice",
 //     });
+//     next();
 //   }
 // };
 
-module.exports = { 
-  show 
+module.exports = {
+  show,
 };
